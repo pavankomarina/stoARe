b6042ceb0d2537e8821e9cc152bd981004dd7294
Pavan Kalyan <komarin@uwindsor.ca>
2021-07-07 15:56:03 +0530

    Update notification method

--- backend/stoare/utils/constants.py
+++ backend/stoare/utils/constants.py
@@ -1,3 +1,3 @@
-EMAIL_SUBJECT = "test email"
+EMAIL_SUBJECT = "Alert! A customer at your store needs assistance."
 EMAIL_MESSAGE = "test message"
 SMS_BODY = "test sms"
--- backend/stoare/utils/serializers.py
+++ backend/stoare/utils/serializers.py
@@ -3,3 +3,4 @@ from rest_framework import serializers
 class MessageSerializer(serializers.Serializer):
     message_type = serializers.CharField()
     shop_id = serializers.IntegerField(default=1)
+    message = serializers.CharField()
